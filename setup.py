e03e68930b6e5587199f43bc781b972178c642c6
Carlos O. POCASANGRE JIMENEZ <35692836+cpocasangre@users.noreply.github.com>
2018-02-02 10:21:36 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ setup(
         'volumetric method',
         'heat in place',
         'geothermal reservoir'],
-    # install_requires=['numpy', 'scipy', 'matplotlib', 'mcerp', 'beautifultable'],
-    classifiers=['Development Status :: 3 - Alpha', 'License :: OSI Approved :: MIT License',
+    install_requires=['numpy', 'scipy', 'matplotlib', 'mcerp', 'beautifultable'],
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'License :: OSI Approved :: MIT License',
                  'Programming Language :: Python :: 2.7'],
     zip_safe=False)
